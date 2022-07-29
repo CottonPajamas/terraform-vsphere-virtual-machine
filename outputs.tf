62fb703517ec92133d40b348ad1a3562acd248e3
CottonPajamas <30998454+CottonPajamas@users.noreply.github.com>
2022-07-29 15:03:39 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "virtual_machine_names" {
   description = "The names of each virtual machine deployed."
 
-  value = ["${flatten(list(
+  value = ["${flatten(tolist([
     vsphere_virtual_machine.virtual_machine_bare.*.name,
     vsphere_virtual_machine.virtual_machine_linux.*.name,
     vsphere_virtual_machine.virtual_machine_windows.*.name,
-  ))}"]
+  ]))}"]
 }
 
 output "virtual_machine_ids" {
   description = "The ID of each virtual machine deployed, indexed by name."
 
   value = "${zipmap(
-    flatten(list(
+    flatten(tolist([
       vsphere_virtual_machine.virtual_machine_bare.*.name,
       vsphere_virtual_machine.virtual_machine_linux.*.name,
       vsphere_virtual_machine.virtual_machine_windows.*.name,
-    )),
-    flatten(list(
+    ])),
+    flatten(tolist([
       vsphere_virtual_machine.virtual_machine_bare.*.id,
       vsphere_virtual_machine.virtual_machine_linux.*.id,
       vsphere_virtual_machine.virtual_machine_windows.*.id,
-    )),
+    ])),
   )}"
 }
 
@@ -29,15 +29,15 @@ output "virtual_machine_default_ips" {
   description = "The default IP address of each virtual machine deployed, indexed by name."
 
   value = "${zipmap(
-    flatten(list(
+    flatten(tolist([
       vsphere_virtual_machine.virtual_machine_bare.*.name,
       vsphere_virtual_machine.virtual_machine_linux.*.name,
       vsphere_virtual_machine.virtual_machine_windows.*.name,
-    )),
-    flatten(list(
+    ])),
+    flatten(tolist([
       vsphere_virtual_machine.virtual_machine_bare.*.default_ip_address,
       vsphere_virtual_machine.virtual_machine_linux.*.default_ip_address,
       vsphere_virtual_machine.virtual_machine_windows.*.default_ip_address,
-    )),
+    ])),
   )}"
 }
